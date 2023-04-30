b0af10edf5a293c6463b47f4394038e60aa32447
Justin <justin@omniscientgeek.com>
2023-04-29 17:00:08 -0700

    WIP to enable SD card

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * FtpServer esp8266 and esp32 with SPIFFS
+ * FtpServer esp8266 and esp32 with SD
  *
  * AUTHOR:  Renzo Mischianti
  *
@@ -7,69 +7,55 @@
  *
  */
 
-#ifdef ESP8266
-#include <ESP8266WiFi.h>
-#elif defined ESP32
 #include <WiFi.h>
-#include "SPIFFS.h"
-#endif
+#include "SD.h"
 
 #include <SimpleFTPServer.h>
 
-const char* ssid = "pettingerV2";
+const char* ssid = "pettingerV@";
 const char* password = "runlion01";
 
 
 FtpServer ftpSrv;   //set #define FTP_DEBUG in ESP8266FtpServer.h to see ftp verbose on serial
 
 void _callback(FtpOperation ftpOperation, unsigned int freeSpace, unsigned int totalSpace){
-  switch (ftpOperation) {
-    case FTP_CONNECT:
-      Serial.println(F("FTP: Connected!"));
-      break;
-    case FTP_DISCONNECT:
-      Serial.println(F("FTP: Disconnected!"));
-      break;
-    case FTP_FREE_SPACE_CHANGE:
-      Serial.printf("FTP: Free space change, free %u of %u!\n", freeSpace, totalSpace);
-      break;
-    default:
-      Serial.printf("Unknown Operation");
-      break;
-  }
+	Serial.print(">>>>>>>>>>>>>>> _callback " );
+	Serial.print(ftpOperation);
+	/* FTP_CONNECT,
+	 * FTP_DISCONNECT,
+	 * FTP_FREE_SPACE_CHANGE
+	 */
+	Serial.print(" ");
+	Serial.print(freeSpace);
+	Serial.print(" ");
+	Serial.println(totalSpace);
+
+	// freeSpace : totalSpace = x : 360
+
+	if (ftpOperation == FTP_CONNECT) Serial.println(F("CONNECTED"));
+	if (ftpOperation == FTP_DISCONNECT) Serial.println(F("DISCONNECTED"));
 };
 void _transferCallback(FtpTransferOperation ftpOperation, const char* name, unsigned int transferredSize){
-  switch (ftpOperation) {
-    case FTP_UPLOAD_START:
-      Serial.println(F("FTP: Upload start!"));
-      break;
-    case FTP_UPLOAD:
-      Serial.printf("FTP: Upload of file %s byte %u\n", name, transferredSize);
-      break;
-    case FTP_TRANSFER_STOP:
-      Serial.println(F("FTP: Finish transfer!"));
-      break;
-    case FTP_TRANSFER_ERROR:
-      Serial.println(F("FTP: Transfer error!"));
-      break;
-    default:
-      break;
-  }
-
-  /* FTP_UPLOAD_START = 0,
-   * FTP_UPLOAD = 1,
-   *
-   * FTP_DOWNLOAD_START = 2,
-   * FTP_DOWNLOAD = 3,
-   *
-   * FTP_TRANSFER_STOP = 4,
-   * FTP_DOWNLOAD_STOP = 4,
-   * FTP_UPLOAD_STOP = 4,
-   *
-   * FTP_TRANSFER_ERROR = 5,
-   * FTP_DOWNLOAD_ERROR = 5,
-   * FTP_UPLOAD_ERROR = 5
-   */
+	Serial.print(">>>>>>>>>>>>>>> _transferCallback " );
+	Serial.print(ftpOperation);
+	/* FTP_UPLOAD_START = 0,
+	 * FTP_UPLOAD = 1,
+	 *
+	 * FTP_DOWNLOAD_START = 2,
+	 * FTP_DOWNLOAD = 3,
+	 *
+	 * FTP_TRANSFER_STOP = 4,
+	 * FTP_DOWNLOAD_STOP = 4,
+	 * FTP_UPLOAD_STOP = 4,
+	 *
+	 * FTP_TRANSFER_ERROR = 5,
+	 * FTP_DOWNLOAD_ERROR = 5,
+	 * FTP_UPLOAD_ERROR = 5
+	 */
+	Serial.print(" ");
+	Serial.print(name);
+	Serial.print(" ");
+	Serial.println(transferredSize);
 };
 
 void setup(void){
@@ -90,22 +76,24 @@ void setup(void){
 
 
   /////FTP Setup, ensure SPIFFS is started before ftp;  /////////
-  
+#define SCK  17
+#define MISO  19
+#define MOSI  23
+#define CS  5
   /////FTP Setup, ensure SPIFFS is started before ftp;  /////////
-#ifdef ESP32       //esp32 we send true to format spiffs if cannot mount
-  if (SPIFFS.begin(true)) {
-#elif defined ESP8266
-  if (SPIFFS.begin()) {
-#endif
+  SPI.begin(SCK, MISO, MOSI, CS);
+
+  if (SD.begin(13, SPI)) {
+      Serial.println("SD opened!");
+
       ftpSrv.setCallback(_callback);
       ftpSrv.setTransferCallback(_transferCallback);
 
-      Serial.println("SPIFFS opened!");
       ftpSrv.begin("esp8266","esp8266");    //username, password for ftp.   (default 21, 50009 for PASV)
-  }    
+  }
 }
 void loop(void){
-  ftpSrv.handleFTP();        //make sure in loop you call handleFTP()!!  
+  ftpSrv.handleFTP();        //make sure in loop you call handleFTP()!!
  // server.handleClient();   //example if running a webserver you still need to call .handleClient();
- 
+
 }
