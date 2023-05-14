d5bd34c777c56f1b3cc62494f0a85c092608cdec
Justin <justin@omniscientgeek.com>
2023-05-13 21:00:05 -0600

    Latest broken version

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,238 @@
 /*
- * FtpServer esp8266 and esp32 with SD
+ * Connect the SD card to the following pins:
  *
- * AUTHOR:  Renzo Mischianti
- *
- * https://www.mischianti.org/2020/02/08/ftp-server-on-esp8266-and-esp32
- *
+ * SD Card | ESP32
+ *    D2       -
+ *    D3       SS
+ *    CMD      MOSI
+ *    VSS      GND
+ *    VDD      3.3V
+ *    CLK      SCK
+ *    VSS      GND
+ *    D0       MISO
+ *    D1       -
  */
-
-#include <WiFi.h>
+#include "FS.h"
 #include "SD.h"
+#include "SPI.h"
 
-#include <SimpleFTPServer.h>
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
 
-const char* ssid = "pettingerV2";
-const char* password = "runlion01";
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
 
-
-FtpServer ftpSrv;   //set #define FTP_DEBUG in ESP8266FtpServer.h to see ftp verbose on serial
-
-void _callback(FtpOperation ftpOperation, unsigned int freeSpace, unsigned int totalSpace){
-	Serial.print(">>>>>>>>>>>>>>> _callback " );
-	Serial.print(ftpOperation);
-	/* FTP_CONNECT,
-	 * FTP_DISCONNECT,
-	 * FTP_FREE_SPACE_CHANGE
-	 */
-	Serial.print(" ");
-	Serial.print(freeSpace);
-	Serial.print(" ");
-	Serial.println(totalSpace);
-
-	// freeSpace : totalSpace = x : 360
-
-	if (ftpOperation == FTP_CONNECT) Serial.println(F("CONNECTED"));
-	if (ftpOperation == FTP_DISCONNECT) Serial.println(F("DISCONNECTED"));
-};
-void _transferCallback(FtpTransferOperation ftpOperation, const char* name, unsigned int transferredSize){
-	Serial.print(">>>>>>>>>>>>>>> _transferCallback " );
-	Serial.print(ftpOperation);
-	/* FTP_UPLOAD_START = 0,
-	 * FTP_UPLOAD = 1,
-	 *
-	 * FTP_DOWNLOAD_START = 2,
-	 * FTP_DOWNLOAD = 3,
-	 *
-	 * FTP_TRANSFER_STOP = 4,
-	 * FTP_DOWNLOAD_STOP = 4,
-	 * FTP_UPLOAD_STOP = 4,
-	 *
-	 * FTP_TRANSFER_ERROR = 5,
-	 * FTP_DOWNLOAD_ERROR = 5,
-	 * FTP_UPLOAD_ERROR = 5
-	 */
-	Serial.print(" ");
-	Serial.print(name);
-	Serial.print(" ");
-	Serial.println(transferredSize);
-};
-
-void setup(void){
-  Serial.begin(115200);
-  Serial.print("Hello World!");
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
- /* while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }*/
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-
-  /////FTP Setup, ensure SPIFFS is started before ftp;  /////////
-#define SCK  17
-#define MISO  23
-#define MOSI  19
-#define CS  5
-  /////FTP Setup, ensure SPIFFS is started before ftp;  /////////
-  SPI.begin(SCK, MISO, MOSI, CS);
-
-  if (SD.begin(13, SPI)) {
-      Serial.println("SD opened!");
-
-      ftpSrv.setCallback(_callback);
-      ftpSrv.setTransferCallback(_transferCallback);
-
-      ftpSrv.begin("esp8266","esp8266");    //username, password for ftp.   (default 21, 50009 for PASV)
-  }
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
 }
-void loop(void){
-  ftpSrv.handleFTP();        //make sure in loop you call handleFTP()!!
- // server.handleClient();   //example if running a webserver you still need to call .handleClient();
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
+
+void setup(){
+    Serial.begin(115200);
+	#define SCK  18
+	#define MISO  19
+	#define MOSI  23
+	#define CS  5
+    pinMode(MISO,INPUT_PULLUP);
+	SPIClass spi = SPIClass(VSPI);
+  Serial.println("SPI Begin");
+	spi.begin(SCK, MISO, MOSI, CS);
+	
+  Serial.println("SD begin");
+if (!SD.begin(CS,spi,80000000)) {
+  Serial.println("Card Mount Failed");
+  Serial.println(SD.cardType());
+  return;
+}
+  Serial.println("Card Mount Success");
+
+    // if(!SD.begin(5)){
+    //     Serial.println("Card Mount Failed");
+    //     return;
+    // }
+    uint8_t cardType = SD.cardType();
+
+    if(cardType == CARD_NONE){
+        Serial.println("No SD card attached");
+        return;
+    }
+
+    Serial.print("SD Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+    listDir(SD, "/", 0);
+    createDir(SD, "/mydir");
+    listDir(SD, "/", 0);
+    removeDir(SD, "/mydir");
+    listDir(SD, "/", 2);
+    writeFile(SD, "/hello.txt", "Hello ");
+    appendFile(SD, "/hello.txt", "World!\n");
+    readFile(SD, "/hello.txt");
+    deleteFile(SD, "/foo.txt");
+    renameFile(SD, "/hello.txt", "/foo.txt");
+    readFile(SD, "/foo.txt");
+    testFileIO(SD, "/test.txt");
+    Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+}
+
+void loop(){
 
 }
